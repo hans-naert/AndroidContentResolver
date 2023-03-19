8cc8e68e55b7684e050c2c8e5ef1965e59212532
Hans Naert <hans.naert@vives.be>
2023-03-19 16:59:23 +0100

    ask permission to read contacts, read and display them

--- app/src/main/java/com/example/androidcontentresolver/MainActivity.kt
+++ app/src/main/java/com/example/androidcontentresolver/MainActivity.kt
@@ -1,10 +1,21 @@
 package com.example.androidcontentresolver
 
+import android.Manifest.permission.READ_CONTACTS
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.androidcontentresolver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val contactsList = ArrayList<String>()
+    private lateinit var adapter: ArrayAdapter<String>
 
     private lateinit var binding: ActivityMainBinding
 
@@ -13,5 +24,49 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        adapter = ArrayAdapter(this, android.R.layout.
+        simple_list_item_1, contactsList)
+        binding.contactsListView.adapter = adapter
+        if (ContextCompat.checkSelfPermission(this, READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(READ_CONTACTS), 1)
+        } else {
+            readContacts()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions,
+            grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    readContacts()
+                } else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+    @SuppressLint("Range")
+    private fun readContacts() {
+        // query the contacts
+        contentResolver.query(
+            ContactsContract.CommonDataKinds.Phone.
+        CONTENT_URI,
+            null, null, null, null)?.apply {
+            while (moveToNext()) {
+                // get contact name
+                val displayName = getString(getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                // get contact phone number
+                val number = getString(getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                contactsList.add("$displayName\n$number")
+            }
+            adapter.notifyDataSetChanged()
+            close()
+        }
     }
 }
